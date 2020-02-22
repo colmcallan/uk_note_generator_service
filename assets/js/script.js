371789651540ffb6457c4aef8be5c34cc7f043c1
colmcallan <42065844+colmcallan@users.noreply.github.com>
2020-02-22 09:35:14 +0000

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -344,21 +344,6 @@ input.addEventListener('click', function () {
   textarea.value = '';
 }, false);
 
-function copytxt() {
-  /* Get the text field */
-  var copyText = document.getElementById("selectedtext");
-
-  /* Select the text field */
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
-
-  /* Copy the text inside the text field */
-  document.execCommand("copy");
-
-  /* Alert the copied text */
-  alert("Copied the text: " + copyText.value);
-}
-
 //refresh page
 function refreshPage() {
   window.location.reload();
